972c06fb425f59e3216290a9f6ddeaa22be56d7a
Taras Labiak <kissarat@gmail.com>
2022-05-22 08:16:05 +0300

    morozov project initial

---
+++ morozov/src/main/java/socex/morozov/App.java
@@ -0,0 +1,13 @@
+package socex.morozov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
